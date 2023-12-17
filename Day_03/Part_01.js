8f316a7111749f2888fd9f5802d6f793c36c53a7
Fabien <86556152+BastosFab@users.noreply.github.com>
2023-12-17 15:16:45 +0100

    🎄Day-3

--- Day_03/Part_01.js
+++ Day_03/Part_01.js
@@ -1,6 +1,44 @@
 const fs = require('fs');
 
-fs.readFile('./exemple.txt', 'utf-8', (err, data) => {
-    const input = data
-    console.log(input);
+const hasSymbol = (str) => {
+    if(str?.length && str.split('').find((s) => isNaN(s) && s !== '.')) return true;
+    return false;
+}
+
+fs.readFile('./input.txt', 'utf-8', (err, data) => {
+    const input = data.split('\n').filter((n) => n);
+
+    const rows = input.length;
+    const columns = input[0].length;
+
+    let founds = [];
+
+    for(let i = 0; i < rows; i++) {
+        for(let j = 0; j < columns; j++) {
+            const number = "" + input[i][j];
+
+            if (isNaN(number)) {
+                continue;
+            }
+
+            let num = number;
+
+            while(++j < columns) {
+                if(Number.isInteger(parseInt(input[i][j]))) {
+                    num += input[i][j];
+                } else break;
+            }
+
+            const top = i === 0 ? "" : input[i - 1].substring(j - num.length - 1, j + 1);
+            const bottom = i === rows - 1 ? "" : input[i + 1].substring(j - num.length - 1, j + 1);
+            const left = input[i][j - num.length - 1] || "";
+            const right = input[i][j] || "";
+
+            // console.log(num, top, bottom, left, right);
+            if (hasSymbol(top) || hasSymbol(bottom) || hasSymbol(left) || hasSymbol(right)) {
+                founds.push(Number(num));
+            }
+        }
+    }
+    console.log(founds.reduce((a, b) => a + b, 0));
 });
--- Day_03/Part_02.js
+++ Day_03/Part_02.js
@@ -1,12 +1,56 @@
 const fs = require('fs');
 
-const conditions = {
-    red: 12,
-    green: 13,
-    blue: 14
-}
+fs.readFile('./input.txt', 'utf-8', (err, data) => {
+    const input = data.split('\n').filter((n) => n);
 
-fs.readFile('./exemple.txt', 'utf-8', (err, data) => {
-    const input = data
-    console.log(input);
+    const rows = input.length;
+    const columns = input[0].length;
+
+    const gearsDic = {};
+
+    const findGears = (str, num, i, j) => {
+        j = j === -1 ? 0 : j;
+        for(let k = 0; k < str.length; k++) {
+            const char = str.charAt(k);
+            if(char === '*') {
+                const ind = `${i}-${j + k}`;
+                gearsDic[ind] = gearsDic[ind] ? [...gearsDic[ind], parseInt(num)] : [parseInt(num)];
+            }
+        }
+    };
+
+    for(let i = 0; i < rows; i++) {
+        for(let j = 0; j < columns; j++) {
+            const number = "" + input[i][j];
+
+            if (isNaN(number)) {
+                continue;
+            }
+
+            let num = number;
+
+            while(++j < columns) {
+                if(Number.isInteger(parseInt(input[i][j]))) {
+                    num += input[i][j];
+                } else break;
+            }
+
+            const top = i === 0 ? "" : input[i - 1].substring(j - num.length - 1, j + 1);
+            const bottom = i === rows - 1 ? "" : input[i + 1].substring(j - num.length - 1, j + 1);
+            const left = input[i][j - num.length - 1] || "";
+            const right = input[i][j] || "";
+
+            findGears(top, num, i-1, j-num.length-1)
+            findGears(bottom, num, i+1, j-num.length-1)
+            findGears(left, num, i, j-num.length-1)
+            findGears(right, num, i, j)
+        }
+    }
+
+    const value = Object.values(gearsDic)
+        .filter((n) => n.length === 2)
+        .map((n) => n[0] * n[1])
+        .reduce((a, b) => a + b, 0);
+
+    console.log(value);
 });
